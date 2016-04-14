f1fd3b2514d6dbfc6a3d5e98637e1aca55264a6b
saadjawed7 <saad.jawed@hotmail.com>
2016-04-14 12:12:25 +0500

    Added files via upload

---
+++ web/index.php
@@ -0,0 +1,309 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="Style.css" type="text/css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<title>UOK</title>
+</head>
+
+<body>
+<div style="margin-left:15%; width:70%;">
+<div class="MyHeader5" style="background-color:#e0e5ca;">
+<div class="MyHeader1">
+</div>
+<nav class="navbar navbar-default" style="background-color:#e0e5ca;">
+  <div class="container-fluid"> 
+  
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#"></a></div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="defaultNavbar1">
+      <ul class="nav navbar-nav" id="MyHeader2">
+        <li><a href="#">Email</a></li>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">MCN</a></li>
+        <li><a href="#">Sitemap</a></li>
+        <li><a href="#">Directory</a></li>
+        </ul>
+        </div>
+        </div>
+        </nav><br/>
+        <div class="container">
+        <div class="row">
+         <div class="col-lg-6" style="float:right;clear:both">
+	  <h3 class="MyNo"></h3></div>
+        <div class="container">
+        <div class="row">
+        
+        <div class="col-lg-6">
+        
+     <div id="MyHeader3" ><img src="img/1.png"  style="padding-bottom:20px;" class="img-responsive" alt="" id="Like" /></div>
+     
+    
+      </div>
+      </div>
+      </div>
+      </div>
+
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <nav class="navbar navbar-default" >
+  <div class="container-fluid"> 
+    <div class="navbar-header" style="float:right; background-color:2B2B2B">
+     <form class="navbar-form navbar-left" role="search" style="display:block-inline">
+        <div class="form-group" >
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default" style="background-color:#526F35;color:white;">Search</button>
+      </form>
+      </div>
+      </div>
+      </nav>
+      </div>
+     </div>
+      
+     
+    <div class="MainBar">
+     <nav class="navbar navbar-default" >
+  <div class="container-fluid"> 
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar2"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#"></a></div>
+    <div class="collapse navbar-collapse" id="defaultNavbar2">
+      <ul class="nav navbar-nav">
+      <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Home<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Campus News</a></li>
+            <li><a href="#">Convocation</a></li>
+            <li><a href="#">Downloads</a></li>
+            <li><a href="#">Our History</a></li>
+          </ul>
+        </li>
+         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admissions<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Morning Test based</a></li>
+            <li><a href="#">PharmD</a></li>
+            <li><a href="#">Master Evening</a></li>
+            <li><a href="#">M.Phil./Ph.D. & M.S./M.D.</a></li>
+            <li><a href="#">International Students</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Faculties<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Education</a></li>
+            <li><a href="#">Engineering</a></li>
+            <li><a href="#">Islamic Studies</a></li>
+            <li><a href="#">Law</a></li>
+            <li><a href="#">Management Science</a></li>
+            <li><a href="#">Medcine</a></li>
+            <li><a href="#">Pharmacy</a></li>
+            <li><a href="#">Sciences</a></li>
+            <li><a href="#">Social Sciences</a></li>
+            <li><a href="#">Institutes</a></li>
+          </ul>
+        </li>
+         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Library<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Digital Library</a></li>
+            <li><a href="#">Ebrary</a></li>
+          </ul>
+        </li>
+         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Administration<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Vice Chancellor</a></li>
+            <li><a href="#">Registrar</a></li>
+            <li><a href="#">Syndicate Members</a></li>
+            <li><a href="#">Former Vice Chancellor</a></li>
+            <li><a href="#">Directorates & Offices</a></li>
+          </ul>
+        </li>
+         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Examination<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Annual System</a></li>
+            <li><a href="#">Downloadable Forms</a></li>
+            <li><a href="#">Comman Questions</a></li>
+            <li><a href="#">Overseas Examination</a></li>
+            <li><a href="#">Semeter System</a></li>
+            <li><a href="#">Semeter Rules Book</a></li>
+          </ul>
+        </li>
+        <li ><a href="#">Alumni</a>
+          
+        </li>
+      </ul>
+      </div>
+      </div>
+      
+      </nav>
+  </div>
+  
+  
+  <div class="row">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+  <div id="hum">
+    <div class="jumbotron">
+      <div class="container">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="http://www.uok.edu.pk/images/ppl3.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="http://www.uok.edu.pk/images/banner4.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="http://www.uok.edu.pk/images/newbanner444.jpg" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="http://www.uok.edu.pk/images/banner4-o.jpg" alt="Flower">
+    </div>
+  </div>
+
+ 
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      </div>
+    </div> 
+    </div>
+  </div>
+  </div>
+<hr color="black">
+
+
+  <div class="row">
+  <div class="col-sm-12">
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="img/2.png">
+    </div>
+  </div>
+  </div>
+  </div>
+  
+  
+  
+  
+  
+  <div class="col-sm-3 text-center">
+     <h3>About Us</h3>
+			 <ul style="text-align:left; font-size:12px">
+     
+          <li><a href="#">Download</a></li>
+          <li><a href="#">MCN</a></li>
+        
+          <li><a href="#">News & events</a></li>
+          <li><a href="#">Picture Gallery</a></li>
+     
+        </ul>
+    </div>
+    <div class="text-center col-sm-3">
+    <h3>Quick Links</h3>
+		 <ul style="text-align:left; font-size:12px">
+     
+          <li><a href="#">Affiliation Committee</a></li>
+          <li><a href="#">Annual Convocation</a></li>
+        
+          <li><a href="#">Directorates & offices</a></li>
+          <li><a href="#">Job Portal</a></li>
+          <li><a href="#">Plagiarism Policy</a></li>
+           <li><a href="#">Physical Education</a></li>
+          <li><a href="#">Tenders and Quotations</a></li>
+        
+          <li><a href="#">Scholarships</a></li>
+          <li><a href="#">Placements and Internships</a></li>
+        </ul>
+      </div>
+    <div class="text-center col-sm-3">
+ <h3>Web Links</h3>
+			<ul style="text-align:left; font-size:12px">
+     
+          <li><a href="#">Digital Library</a></li>
+          <li><a href="#">Download Center HEC-Microsoft</a></li>
+        
+          <li><a href="#">Higher Education Commission</a></li>
+          <li><a href="#">Quality Enhancement Cell</a></li>
+          <li><a href="#">Directory</a></li>
+        </ul>
+    </div>
+  
+    <div class="col-sm-3 text-center">
+     
+			 
+     
+          	<h3>V.C. Office</h3>
+			
+     
+          <p style="text-align:left; font-size:12px">Inquiry Direct: 99261300-07 Ext: 2219</p>
+          <p style="text-align:left; font-size:12px">Fax: 99261340, 99261330 (P.R.O.)</p>
+      <p style="text-align:left; font-size:12px">Email: vc@uok.edu.pk</p>
+     
+    </div><br>
+    <hr>
+    <nav class="navbar navbar-default" style="clear:both">
+  <div class="container-fluid"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar3"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#"></a></div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="defaultNavbar3">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Credits</a></li>
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">Copyright</a></li>
+        <li><a href="#">Disclaimer</a></li>
+        <li><a href="#">Maps</a></li>
+        </ul>
+        </div>
+        </div>
+        </nav>
+        
+    
+    
+   
+
+  
+        
+
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+     <script>
+  
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
+});
+  </script>
+  </div>
+</body>
+</html>
